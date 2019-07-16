8af1589dd2e5c2dee6c1d762381519f676c2a1ac
CalumCannon <calbo90@googlemail.com>
2019-07-16 09:53:28 +0100

    added create_router.js

---
+++ server/helpers/create_router.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+
+const createRouter = function(collection){
+
+  const router = express.Router();
+
+  //Get
+  router.get('/', (req, res) => {
+    collection
+    .find()
+    .then((docs) => res.json(docs))
+    .catch((err) => {
+    console.error(err);
+    res.status(500);
+    res.json({ status: 500, error: err });
+   });
+  })
+  
+
+
+}

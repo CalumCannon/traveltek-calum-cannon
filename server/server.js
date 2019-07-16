51e0c3d91ee4c706c71de877ed81ead59d7f5b67
CalumCannon <calbo90@googlemail.com>
2019-07-16 14:44:32 +0100

    added server.js

--- server/server.js
+++ server/server.js
@@ -4,5 +4,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 app.use(cors())
+app.use(bodyParser.json());
 
-//const MongoClient = require('mongodb')
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+.then(() => {
+const db = client.db('flights_db');
+const flightsCollection = db.collection('flights')
+app.use('api/flights/', createRouter(flightsCollection));
+
+}).catch(console.error);
+
+app.listen(3000, function(){
+  console.log('server running');
+});
